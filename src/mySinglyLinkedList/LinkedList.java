40b9f0c600b44e9d559df6545574722487f88932
Koushik579 <koushikkarmakar535@gmail.com>
2025-12-27 23:47:31 +0530

    size() and insertAt() added

--- src/mySinglyLinkedList/LinkedList.java
+++ src/mySinglyLinkedList/LinkedList.java
@@ -9,41 +9,72 @@ package mySinglyLinkedList;
  * @author username
  */
 public class LinkedList<T> {
-   
+
     private Node<T> head;
-    
-    public void insert(T data)
-    {
+
+    public void insert(T data) {
         Node<T> node = new Node<T>();
         node.data = data;
         node.next = null;
-        
-        if(head == null)
-        {
+
+        if (head == null) {
             head = node;
-        }
-        else
-        {
+        } else {
             Node<T> tempnode = head;
-            while(tempnode.next != null)
-            {
+            while (tempnode.next != null) {
                 tempnode = tempnode.next;
             }
             tempnode.next = node;
         }
     }
-    
-    public void show()
-    {
+
+    public void insertAt(int index,T data) {
+        Node<T> node = new Node<T>();
+        node.data = data;
+        if(index > size()-1)
+        {
+            throw new IndexOutOfBoundsException("Index must be within the size of the list");
+        }
+        else
+        {
+            Node<T> tempnode = head;
+            int count = 0 ;
+            while(tempnode.next != null)
+            {
+                count++;
+                if(count == index-1)
+                {
+                    node.next = tempnode.next;
+                    tempnode.next = node;
+                    return;
+                }
+                tempnode = tempnode.next;
+            }
+            
+        }
+    }
+
+    public void show() {
         Node<T> tempnode = head;
         StringBuilder sb = new StringBuilder("[ ");
-        while(tempnode != null)
-        {
+        while (tempnode != null) {
             sb.append(tempnode.data);
-            if(tempnode.next != null)sb.append(" , ");
+            if (tempnode.next != null) {
+                sb.append(" , ");
+            }
             tempnode = tempnode.next;
         }
         sb.append(" ]");
         System.out.println(sb.toString());
-    }   
+    }
+
+    public int size() {
+        int count = 0;
+        Node<T> tempnode = head;
+        while (tempnode != null) {
+            count++;
+            tempnode = tempnode.next;
+        }
+        return count;
+    }
 }
