9918d8b145ac3ee87103ae13914b505bd4accba1
Koushik579 <koushikkarmakar535@gmail.com>
2026-01-08 12:31:53 +0530

    data getter and check data

--- src/mySinglyLinkedList/LinkedList.java
+++ src/mySinglyLinkedList/LinkedList.java
@@ -78,4 +78,97 @@ public class LinkedList<T> {
         }
         return count;
     }
+
+    public void reverse() {
+        Node<T> pre = null;
+        Node<T> current = head;
+        while (current != null) {
+            if (current.next == null) {
+                head = current;
+            }
+            Node next = current.next;
+            current.next = pre;
+            pre = current;
+            current = next;
+        }
+    }
+
+    public void middle() {
+        Node<T> tempnode1 = head;
+        Node<T> tempnode2 = head;
+        if (head == null) {
+            throw new IllegalStateException("No data in the list");
+        } else {
+            while (tempnode1 != null) {
+                tempnode1 = tempnode1.next;
+                if (tempnode1 != null) {
+                    tempnode1 = tempnode1.next;
+                    tempnode2 = tempnode2.next;
+                }
+            }
+        }
+        System.out.println("Middle : " + tempnode2.data);
+    }
+
+    public void delete(int index) {
+        Node tempnode = head;
+        Node pre = null;
+        boolean isdelete = false;
+        int count = 0;
+        if (index == 0) {
+            if (head != null) {
+                tempnode = tempnode.next;
+                head = tempnode;
+            } else {
+                throw new IndexOutOfBoundsException("No data in the list");
+            }
+        } else {
+            while (tempnode != null) {
+                if (count == index) {
+                    pre.next = tempnode.next;
+                    isdelete = true;
+                    return;
+                }
+                pre = tempnode;
+                tempnode = tempnode.next;
+                count++;
+            }
+            if (isdelete == false) {
+                throw new IndexOutOfBoundsException("Index range out of the list");
+            }
+        }
+    }
+
+    public int getData(int index) {
+        int count = 0;
+        int data = 0;
+        if (index > size() - 1) {
+            throw new IndexOutOfBoundsException("Index not present in the list");
+        } else {
+            Node tempnode = head;
+            while (tempnode != null) {
+                if (count == index) {
+                    data = (int) tempnode.data;
+                    return data;
+                }
+                tempnode = tempnode.next;
+                count++;
+
+            }
+        }
+        return data;
+    }
+
+    public boolean isPresent(int data) {
+        Node tempnode = head;
+        boolean ispresent = false;
+        while (tempnode != null) {
+            if (data == (int) tempnode.data) {
+                ispresent = true;
+                return ispresent;
+            }
+            tempnode = tempnode.next;
+        }
+        return ispresent;
+    }
 }
